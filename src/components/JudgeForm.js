7092f46effa393a0db501f1cae1b16aaabfc1366
joshua david jennings <jennings.josh@gmail.com>
2022-04-16 15:40:01 -0500

    added search to judge's form

--- src/components/JudgeForm.js
+++ src/components/JudgeForm.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Search from './Search';
 
-function JudgeForm() {
+function JudgeForm({ search, handleSearch }) {
 	return (
 		<section id="judgeForm">
 			<h2>Judge's Form</h2>
 			<p>Enter Solo Adjudications Here</p>
-			<div></div>
+			<div>
+				<Search search={search} handleSearch={handleSearch} />
+			</div>
 		</section>
 	);
 }
