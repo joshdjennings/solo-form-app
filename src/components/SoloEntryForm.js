fd0a03da1dc959d1ae907122ea5dec02c77b446e
joshua david jennings <jennings.josh@gmail.com>
2022-04-25 22:48:23 -0500

    thought I added form content but it doesn't work.

--- src/components/SoloEntryForm.js
+++ src/components/SoloEntryForm.js
@@ -1,69 +1,71 @@
 import React from 'react';
-// import Search from './Search';
 
-function SoloEntryForm({ addNewSolo }) {
-	// const defaultState = {
-	// 	name: '',
-	// 	title: '',
-	// 	instrument: '',
-	// };
+function SoloEntryForm({ solo, onChangeForm, addNewSolo }) {
+	function handleInputChange(event) {
+		onChangeForm(event.target.name, event.target.value);
+	}
 
-	// const [formData, setFormData] = useState(defaultState);
+	function handleSubmit(event) {
+		event.preventDefault();
+		fetch(`http://localhost:3005/solos/${solo.id}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(solo),
+		}).then((r) => r.json());
+		// .then(onEditSolo);
+	}
 
-	// const handleChange = (e) => {
-	// 	const thingWeChange = e.target.name;
-	// 	setFormData(() => ({ ...formData, [thingWeChange]: e.target.value }));
-	// };
+	if (!solo) return null;
+
+	const { name, title, instrument } = solo;
 
 	return (
 		<div>
-			<section id="soloEntryForm">
-				<h2>Solo Entry Form</h2>
-				<p>Enter Solo Information Here</p>
+			<form onSubmit={handleSubmit}>
+				<div className="form-row">
+					<div className="col-5">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Student Name"
+							name="name"
+							value={name}
+							onChange={handleInputChange}
+						/>
+					</div>
+					<div className="col-5">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Solo Title"
+							name="title"
+							value={title}
+							onChange={handleInputChange}
+						/>
+					</div>
+					<div className="col">
+						<select
+							name="instrument"
+							value={instrument}
+							onChange={handleInputChange}
+							className="form-control"
+						>
+							<option value="Marimba">Marimba</option>
+							<option value="Vibraphone">Vibraphone</option>
+							<option value="Snare Drum">Snare Drum</option>
+							<option value="Other">Other</option>
+						</select>
+					</div>
 
-				{/* <Search /> */}
-
-				<h3>Add a Solo!</h3>
-				{/* <Form
-				onSubmit={(e) => {
-					e.preventDefault();
-					console.log('submitting form...');
-
-					// reset the form
-					setFormData(defaultState);
-					addNewSolo(formData);
-				}}
-			>
-
-				<Form.Group widths="equal">
-					<Form.Input
-						fluid
-						label="Name"
-						value={formData.name}
-						onChange={handleChange}
-						placeholder="Name"
-						name="name"
-					/>
-					<Form.Input
-						fluid
-						label="title"
-						value={formData.title}
-						onChange={handleChange}
-						placeholder="title"
-						name="title"
-					/>
-					<Form.Input
-						fluid
-						label="instrument"
-						value={formData.instrument}
-						onChange={handleChange}
-						placeholder="instrument"
-						name="instrument"
-					/>
-				</Form.Group>
-				<Form.Button>Submit</Form.Button>
-			</Form> */}
-			</section>
+					<div className="col">
+						<button type="submit" className="btn btn-success">
+							Submit
+						</button>
+					</div>
+				</div>
+			</form>
 		</div>
 	);
 }
