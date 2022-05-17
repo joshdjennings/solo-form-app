74e6695764adb0ffb4d18dea17264f5910f82624
joshua david jennings <jennings.josh@gmail.com>
2022-05-17 12:58:05 -0500

    removed <switch>

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import NavBar from './NavBar';
 import Home from './Home';
 import SoloEntryForm from './SoloEntryForm';
@@ -26,23 +26,18 @@ function App() {
 			}}
 		>
 			<NavBar />
-			<Switch>
-				<Route path="/entryform">
-					<SoloEntryForm setSolos={setSolos} />
-				</Route>
-				<Route path="/sololist">
-					<SoloList solos={solos} />
-				</Route>
-				<Route path="/results">
-					<Results solos={solos} />
-				</Route>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="*">
-					<h1>404 not found</h1>
-				</Route>
-			</Switch>
+			<Route path="/entryform">
+				<SoloEntryForm setSolos={setSolos} />
+			</Route>
+			<Route path="/sololist">
+				<SoloList solos={solos} />
+			</Route>
+			<Route path="/results">
+				<Results solos={solos} />
+			</Route>
+			<Route exact path="/">
+				<Home />
+			</Route>
 		</div>
 	);
 }
--- src/components/SoloCard.js
+++ src/components/SoloCard.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 
 function SoloCard({ solos: { id, name, title, instrument, inst_pic } }) {
+	// function handleDelete() {
+	// 	fetch(`http://localhost:3005/solos/${id}`, {
+	// 		method: 'DELETE',
+	// 	})
+	// 		.then((r) => r.json())
+	// 		.then(() => {
+	// 			onDeleteListing(id);
+	// 		});
+	// }
 	return (
 		<div className="card">
 			<Card>
-				<Image src={inst_pic} />
+				<Image src={inst_pic} alt="Sorry, no image available." />
 				<div className="content">
 					<div className="name">{name}</div>
 					<div className="title">{title}</div>
 					<div className="instrument">{instrument}</div>
+					{/* <button onClick={handleDelete}>Delete Entry</button> */}
 				</div>
 			</Card>
 		</div>
